34f43f815c9b1de4933d7638f457082feee1d611
maaz80 <maazforlap@gmail.com>
2026-08-20 13:25:24 +0530

    Added env files

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-const LIVE_API_URL = "https://shiksha-backend-jlmj.onrender.com/api";
+const LIVE_API_URL = "https://api.shikshadesign.com/api";
+// const LIVE_API_URL = "http://localhost:5000/api";
 
 const getApiUrl = () => {
      let baseUrl = (process.env.NEXT_PUBLIC_API_URL || process.env.VITE_API_URL || LIVE_API_URL).trim().replace(/\/$/, "");
